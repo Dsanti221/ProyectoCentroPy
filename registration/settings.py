47ed2897e3c8960678291922391484c137634c37
Dsanti221 <david2vallejo@gmail.com>
2023-05-18 10:56:21 -0500

    Se actualizo y va a servir

--- registration/settings.py
+++ registration/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-f&25drjcakda$z+5$21t$atgey4^32li0yv)vlstpk$m*e2yx4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['finalappcentro.azurewebsites.net']
 
 
 # Application definition
